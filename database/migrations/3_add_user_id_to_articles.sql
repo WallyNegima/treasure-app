cec1982bd0cd025e88a105823cb780d9f591a18c
seven320 <yosyuaomenw@yahoo.co.jp>
2019-08-13 10:56:50 +0900

    change migrate

--- database/migrations/3_add_user_id_to_articles.sql
+++ database/migrations/3_add_user_id_to_articles.sql
@@ -1,8 +1,6 @@
 -- +goose Up
-ALTER TABLE article ADD column user_id int(10) default NULL;
+ALTER TABLE article ADD column user_id int(10) UNSIGNED NOT NULL;
 
--- 
--- ALTER TABLE article ADD FOREIGN KEY(user_id) REFERENCES user(id);
-
+ALTER TABLE article ADD CONSTRAINT article_fk_user FOREIGN KEY(user_id) REFERENCES user(id);
 
 -- +goose Down
