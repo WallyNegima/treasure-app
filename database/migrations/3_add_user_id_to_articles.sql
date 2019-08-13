53d6245a3ebea5d2df380078bee5116dd8de3130
wally <u.str.gm@gmail.com>
2019-08-13 11:11:13 +0900

    fix: migration file

--- database/migrations/3_add_user_id_to_articles.sql
+++ database/migrations/3_add_user_id_to_articles.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-ALTER TABLE article ADD column user_id int(10) UNSIGNED NOT NULL;
+ALTER TABLE article ADD column user_id int(10) UNSIGNED DEFAULT NULL;
 
 ALTER TABLE article ADD CONSTRAINT article_fk_user FOREIGN KEY(user_id) REFERENCES user(id);
 
