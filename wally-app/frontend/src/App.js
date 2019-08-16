e3362bbc64b3e7fef51265d0936685277a15ed44
wally <u.str.gm@gmail.com>
2019-08-16 14:12:03 +0900

    chore: add frontend base

--- wally-app/frontend/src/App.js
+++ wally-app/frontend/src/App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from "./components/commons/Header";
+import Top from "./pages/Top";
+import Training from "./pages/Training";
+import Result from "./pages/Result";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Router>
+        <Route exact path="/" component={Top} />
+        <Route exact path="/training" component={Training} />
+        <Route exact path="/result" component={Result} />
+      </Router>
     </div>
   );
 }
