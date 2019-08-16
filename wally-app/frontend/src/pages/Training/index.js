d613b18ab9eba168ee3158219fce8f5d2c553908
wally <u.str.gm@gmail.com>
2019-08-16 15:53:23 +0900

    style: trainingなうページのスタイルを整えた

--- wally-app/frontend/src/pages/Training/index.js
+++ wally-app/frontend/src/pages/Training/index.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Button from "components/commons/Button";
+import { Container, Count } from "./styles";
 const Top = () => {
-  return <div>あああ</div>;
+  return (
+    <Container>
+      <Count>1回</Count>
+      <Button>限界突破！懸垂を終了する</Button>
+    </Container>
+  );
 };
 
 export default Top;
---
+++ wally-app/frontend/src/pages/Training/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 20px auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex-wrap: wrap;
+`;
+
+export const Count = styled.div`
+  font-size: 32px;
+  font-weight: bold;
+  margin: 24px auto;
+`;
