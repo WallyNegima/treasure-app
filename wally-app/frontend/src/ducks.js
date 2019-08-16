53dbf0437a2a55f4ed287723aad5a9a34b3a9703
wally <u.str.gm@gmail.com>
2019-08-16 16:54:25 +0900

    chore: add ducks

---
+++ wally-app/frontend/src/ducks.js
@@ -0,0 +1,17 @@
+import { createActions, handleActions } from "redux-actions";
+
+const defaultState = {
+  workLogId = null
+}
+
+export const {
+  overwriteWorkLogId
+} = createActions({
+  OVERWRITE_WORK_LOG_ID: workLogId => ({workLogId})
+})
+
+export default handleActions({
+  [overwriteWorkLogId]: (state, { payload: {workLogId}}) => {
+    return {...state, workLogId}
+  }
+})
